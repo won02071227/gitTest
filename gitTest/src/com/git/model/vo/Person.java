a6777bfc7403b271047542a679d7baea105c24f9
won <won@DESKTOP-G2QCOTT>
2019-08-20 13:49:00 +0900

    model객체 추가 by oh

---
+++ gitTest/src/com/git/model/vo/Person.java
@@ -0,0 +1,36 @@
+package com.git.model.vo;
+
+public class Person {
+	private String name;
+	private int age;
+    private String address;
+    
+    public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+    
+}
--- gitTest/src/com/git/run/Main.java
+++ gitTest/src/com/git/run/Main.java
@@ -3,8 +3,8 @@ package com.git.run;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕 내가 만든 출력");
-		String name="유병승";
+		// TODO Auto-generated method stub
+
 	}
 
 }
